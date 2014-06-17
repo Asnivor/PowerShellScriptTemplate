ab979d1dc78ec994a6994807032c7c311faa59cd
asnivor <coding@asnitech.co.uk>
2014-06-17 10:24:36 +0100

    Initial Commit

---
+++ ScriptTemplate.ps1
@@ -0,0 +1,95 @@
+﻿
+<#
+.SYNOPSIS
+  	<Script Overview>
+ 
+.DESCRIPTION
+  	<Script Description>
+  
+.PARAMETER <Parameter_Name>
+    <Command line Parameter information>
+ 
+.INPUTS
+	<External files or sources that script needs>
+	 
+.OUTPUTS
+  	<Outputs if any>
+ 
+.NOTES
+  	Version:        
+	Author:         
+	Creation Date:  
+	Purpose/Change: 
+  
+.EXAMPLE
+	<Example goes here. Repeat this attribute for more than one example>
+#>
+
+#------------------------------------------------------[Command Line Switches]-----------------------------------------------------
+
+param
+(
+	[switch]$debug
+)
+ 
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+ 
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+ 
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+ 
+#Script Version
+$sScriptVersion = "1.0"
+ 
+ 
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+ 
+Function <FunctionName>
+{
+<#
+.SYNOPSIS
+			<What the function does>
+.DESCRIPTION
+			<Description of function>
+.PARAMETER
+			<Input params and descriptions>
+.RETURNS
+			<What the function returns>
+.EXAMPLE
+			<Function example>
+#>
+  	Param
+	(
+	
+	)  
+  	Begin
+	{
+    	
+ 	}  
+  	Process
+	{
+    	Try
+		{
+      		<code goes here>
+    	}    
+    	Catch
+		{
+      		
+      		Break
+    	}
+  	}  
+ 	End
+	{
+    	If($?)
+		{
+		    
+    	}
+  	}
+}
+ 
+ 
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+ 
+#Code Goes Here
+
